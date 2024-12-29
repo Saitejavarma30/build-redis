af576c18316104467c9505a766b89381a15e4147
Saiteja Varma <saitejavarmaj30@gmail.com>
2024-12-29 12:50:13 +0530

    ping multiple

--- app/main.ts
+++ app/main.ts
@@ -5,9 +5,23 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
-    connection.write('+PONG\r\n');
+    let bufferCommand = '';
 
+    connection.on('data', (data) => {
+        bufferCommand += data.toString();
+        if (bufferCommand.endsWith('\r\n')) {
+            const command = bufferCommand.trim();
+            bufferCommand = '';
+            if (command === 'PING') {
+                connection.write('+PONG\r\n');
+            } else if (command.startsWith('ECHO')) {
+                const message = command.substring(5).trim();
+                connection.write(`+${message}\r\n`);
+            } else {
+                connection.write('-ERR unknown command\r\n');
+            }
+        }
+    });
 });
 //
 server.listen(6379, "127.0.0.1");
